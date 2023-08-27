d4e9b44556bc3fd5a3668e54ac575e5eccc069a7
Shauryaditya <shauryadityab@gmail.com>
2023-08-27 23:18:31 +0530

    minor changes

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -17,17 +17,14 @@ const Contact = () => {
         <form class="w-full max-w-full mt-8">
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full md:w-1/2 px-3 mb-6 md:mb-0">
-          
               <input
                 class="appearance-none block w-full bg-transparent text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
                 id="grid-first-name"
                 type="text"
                 placeholder="Name"
               />
-            
             </div>
             <div class="w-full md:w-1/2 px-3">
-           
               <input
                 class="appearance-none block w-full bg-transparent text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-last-name"
@@ -38,16 +35,13 @@ const Contact = () => {
           </div>
           <div class="flex flex-wrap -mx-3 mb-6">
             <div class="w-full  px-3">
-             
-              <input
-                class="appearance-none block w-full bg-transparent text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+              <textarea
+                class="appearance-none block w-full h-36 bg-transparent text-gray-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
                 id="grid-password"
                 type="text"
                 placeholder="Type question here"
               />
-             
-          
-         </div>
+            </div>
           </div>
         </form>
       </div>
