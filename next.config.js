9ff6428bb4a2b91c7a5528cade7bd395b0be35fd
Debmalya Biswas <viperbale.db@gmail.com>
2023-09-05 09:23:38 +0530

    added records

--- next.config.js
+++ next.config.js
@@ -1,12 +1,21 @@
-/** @type {import('next').NextConfig} */
+const path = require("path");
+
 module.exports = {
-    webpack(config) {
-      config.module.rules.push({
-        test: /\.svg$/i,
-        issuer: /\.[jt]sx?$/,
-        use: ['@svgr/webpack'],
-      })
-  
-      return config
-    },
-  }
+  webpack(config) {
+    config.module.rules.forEach((rule) => {
+      const { oneOf } = rule;
+      if (oneOf) {
+        oneOf.forEach((one) => {
+          if (!`${one.issuer?.and}`.includes("_app")) return;
+          one.issuer.and = [path.resolve(__dirname)];
+        });
+      }
+    });
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
+};
--- src/components/Dashboard/NotesandInstructions.jsx
+++ src/components/Dashboard/NotesandInstructions.jsx
@@ -1,66 +1,57 @@
-'use client'
-import React,{useState} from 'react'
-import Notes from './Notes'
-import Instructions from './Instructions'
-import Alaysis from './Alaysis'
+"use client";
+import React, { useState } from "react";
+import Notes from "./Notes";
+import Instructions from "./Instructions";
+import Alaysis from "./Alaysis";
 
 const NotesandInstructions = () => {
-    const heading = [
-        {
-            id: '8uhtyuujhg6987yhrtyhytg',
-            name: 'Notes'
-        },
-        {
-            id: '8uhty45656uujhg6987yhrtyhytg',
-            name: 'Instructions'
-        },
-        {
-            id: '8uhtyrt436987yhrtyhytg',
-            name: 'Alaysis'
-        },
-     
-    ]
-    const [selectedTab, setSelectedTab] = useState(1)
+  const heading = [
+    {
+      id: "8uhtyuujhg6987yhrtyhytg",
+      name: "Notes",
+    },
+    {
+      id: "8uhty45656uujhg6987yhrtyhytg",
+      name: "Instructions",
+    },
+    {
+      id: "8uhtyrt436987yhrtyhytg",
+      name: "Alaysis",
+    },
+  ];
+  const [selectedTab, setSelectedTab] = useState(1);
 
-    return (
-        <div className='flex gap-14 p-10'>
-            
-            <div className='grow flex flex-col'>
-               
-                <div className='max-w-xs flex justify-between bg-[#2F303D] rounded-lg overflow-hidden'>
-                    {
-                        heading.map((item, index) => (
-                            <div
-                                onClick={() => setSelectedTab(index + 1)}
-                                key={item.id} className={`w-1/3 h-10 flex justify-center rounded-lg items-center cursor-pointer text-sm ${selectedTab === index + 1 ? 'text-[#8167E6] bg-white' : 'text-gray-500 '}`}>{item.name}</div>
-                        ))
-                    }
-                     {/* <div className="flex">
+  return (
+    <div className="flex gap-14 p-10">
+      <div className="grow flex flex-col">
+        <div className="max-w-xs flex justify-between bg-[#2F303D] rounded-lg overflow-hidden">
+          {heading.map((item, index) => (
+            <div
+              onClick={() => setSelectedTab(index + 1)}
+              key={item.id}
+              className={`w-1/3 h-10 flex justify-center rounded-lg items-center cursor-pointer text-sm ${
+                selectedTab === index + 1
+                  ? "text-[#8167E6] bg-white"
+                  : "text-gray-500 "
+              }`}
+            >
+              {item.name}
+            </div>
+          ))}
+          {/* <div className="flex">
                     <button className='text-xs text-white bg-[#2F303D] rounded-md px-4 py-2'>Save</button>
                     <button className='text-xs text-white bg-[#2F303D] rounded-md px-4 py-2'>Save</button>
                 </div> */}
-                </div>
-               
-              
-                <div className='p-0 mt-4'>
-                    {
-                        selectedTab === 2 ?
-                            <Instructions /> : null
-                    }
-                    {
-                        selectedTab === 1 ?
-                            <Notes /> : null
-                    }
-                    {
-                        selectedTab === 3 ?
-                            <Alaysis /> : null
-                    }
-                </div>
-            </div>
         </div>
-    )
-}
 
-  
+        <div className="p-0 mt-4">
+          {
+            selectedTab === 1? <Notes/> : selectedTab === 2? <Instructions/> : <Alaysis/>
+          }
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NotesandInstructions
+export default NotesandInstructions;
