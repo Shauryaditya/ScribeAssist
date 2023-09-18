7fefe1f08ba26876a50e1b43a6f499cf36c1b475
neo062 <navinkumar241999@gmail.com>
2023-09-18 12:04:30 +0530

    hello there

---
+++ src/app/success/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ThankYou from '../../components/ThankYou'
+const page = () => {
+    return (
+        <>
+            <ThankYou />
+        </>
+    )
+}
+
+export default page
